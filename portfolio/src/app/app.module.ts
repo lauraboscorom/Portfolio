4a5a099507f383661f9954268f9f9ce85298cb00
Laura Bosco <laurabosrom@gmail.com>
2022-03-30 13:34:01 +0200

    Translation and contact section

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,17 +6,40 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { ContactComponent } from './components/contact/contact.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
   ],
-  providers: [],
+  providers: [
+    TranslateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // Languages variables
+  languages: string[];
+  translateService: TranslateService;
+  currentLang: string;
+
+  constructor(translateService: TranslateService) {
+    this.translateService = translateService;
+  }
 
   ngOnInit(): void {
+    // Languages variables assignments
+    this.languages = this.translateService.getLangs();
+    this.currentLang = this.translateService.getDefaultLang();
+  }
+
+  /**
+   * Takes the given language and assigns it to the translate service as current language
+   * @param language chosen language
+   */
+  useLanguage(language: string): void {
+    this.translateService.use(language);
+    this.currentLang = this.translateService.currentLang;
   }
 
 }
